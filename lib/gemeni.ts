1e0c917ceafcc16e9e74f5d33902cc44e391dda4
sofyan <soufianeboukir0@gmail.com>
2025-07-23 19:40:41 +0100

    integrating gemeni ai

--- lib/gemeni.ts
+++ lib/gemeni.ts
@@ -1,9 +1,9 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
 
 export async function askGemini(prompt: string) {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const model = genAI.getGenerativeModel({ model:"gemini-2.0-flash-001" });
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
